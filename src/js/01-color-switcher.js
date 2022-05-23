8a9283d48753e79c77889e3ef41f5f052cb8a8a1
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-23 22:37:37 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStartRef = document.querySelector('button[data-start]');
+const buttonStopRef = document.querySelector('button[data-stop]');
+let timerId = null;
 
+
+buttonStartRef.addEventListener('click', onButtonStart);
+buttonStopRef.addEventListener('click', onButtonStop);
+
+
+
+function onButtonStart() {
+  
+     timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+     }, 1000);
+    buttonStartRef.disabled = true;
+    buttonStopRef.disabled = false;
+}
+
+function onButtonStop() {
+    clearInterval(timerId);
+    buttonStartRef.disabled = false;
+    buttonStopRef.disabled = true;
+    
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
