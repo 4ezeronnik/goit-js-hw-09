4223e29bffb904dc6669fc32c9f75f0e87c2f824
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-24 11:14:13 +0300

    updated partly 2 task

---
+++ src/js/02-timer.js
@@ -0,0 +1,26 @@
+
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+
+const buttonStartRef = document.querySelector('button[data-start]');
+const inputRef = document.querySelector('input[type="text"]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const fp = flatpickr(inputRef, options);
+
+buttonStartRef.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+    
+}
+
