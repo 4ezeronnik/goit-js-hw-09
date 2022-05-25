0568b4f0deb68c97264e09d260e1e04e20dc36cc
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-25 17:42:34 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,31 @@
+const formRef = document.querySelector('form');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+formRef.addEventListener('input', onFormInput);
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) {
+   const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+   
+    formData[evt.target.name] = evt.target.value;
+     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const checkedData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || '';
+  if (!checkedData.delay || !checkedData.step || !checkedData.amount)  {  
+        return
+}
+    console.log(localStorage.getItem(STORAGE_KEY));
+    evt.target.reset();
+    localStorage.removeItem(STORAGE_KEY);
+ 
+  
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +34,5 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
